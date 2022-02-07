58b19aad31949d343ef7d961072922e9ddce765c
Houmame LAZAR <houms937@gmail.com>
2022-02-07 17:15:51 +0100

    fffffffffffffffff

---
+++ src/main/java/fr/iutmontreuil/csid/monpetitbonsaiprivate/owner/CareEventMapper.java
@@ -0,0 +1,34 @@
+package fr.iutmontreuil.csid.monpetitbonsaiprivate.owner;
+
+import fr.iutmontreuil.csid.monpetitbonsaiprivate.commons.persistence.CareEventEntity;
+import fr.iutmontreuil.csid.monpetitbonsaiprivate.commons.persistence.CareType;
+import fr.iutmontreuil.csid.monpetitbonsaiprivate.owner.domain.model.CareEvent;
+import fr.iutmontreuil.csid.monpetitbonsaiprivate.owner.exposition.dto.CareEventDto;
+
+public class CareEventMapper {
+
+    public static CareEvent mapDTOtoModel(CareEventDto careEventDTO) {
+        return new CareEvent(careEventDTO.getId(), careEventDTO.getCareDate(), OwnerMapper.mapDtoToModelOwner(careEventDTO.getOwner()), OwnerMapper.mapDtoToModelBonsai(careEventDTO.getBonsai()), careEventDTO.getCareType());
+    }
+
+    public static CareEventEntity mapModelToEntity(CareEvent careEvent) {
+        CareEventEntity careEventEntity = new CareEventEntity();
+        careEventEntity.setId(careEvent.getId());
+        careEventEntity.setCareDate(careEvent.getCareDate());
+        careEventEntity.setOwner(OwnerMapper.mapModelToEntityOwner(careEvent.getOwner()));
+        careEventEntity.setBonsai(OwnerMapper.mapModelToEntityBonsai(careEvent.getBonsai()));
+        careEventEntity.setCareType(careEvent.getCareType());
+        return careEventEntity;
+    }
+
+
+    public static CareEvent mapEntityToModel(CareEventEntity careEventEntity) {
+        return new CareEvent(careEventEntity.getId(), careEventEntity.getCareDate(), OwnerMapper.mapEntityToModelOwner(careEventEntity.getOwner()), OwnerMapper.mapEntityToModelBonsai(careEventEntity.getBonsai()), careEventEntity.getCareType());
+    }
+
+    public static CareEventDto mapModelToDto(CareEvent careEvent) {
+        return new CareEventDto(careEvent.getId(), careEvent.getCareDate(), OwnerMapper.mapModelToDtoOwner(careEvent.getOwner()), OwnerMapper.mapModelToDtoBonsai(careEvent.getBonsai()), careEvent.getCareType());
+    }
+
+
+}
